e40f5c34021cdf9bfb01d3ed453fe264873399fd
magallegos1996 <bwcekos@gmail.com>
2020-04-28 14:55:54 -0500

    Revert "Instalación y cofiguracion de Vue"
    This reverts commit 0480ebd239ebaa45256f173f80721d6057a5cb6d.

--- src/app/Components/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="container">
-        <nav class="navbar navbar-light bg-light">
-            <a href="/" class="navbar-brand">INICIO</a>
-        </nav>
-    </div>
-</template>
