0480ebd239ebaa45256f173f80721d6057a5cb6d
magallegos1996 <bwcekos@gmail.com>
2020-04-27 21:17:41 -0500

    Instalación y cofiguracion de Vue
    Usando Webpack y babel

---
+++ src/app/Components/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container">
+        <nav class="navbar navbar-light bg-light">
+            <a href="/" class="navbar-brand">INICIO</a>
+        </nav>
+    </div>
+</template>
