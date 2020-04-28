9bd2bcd7cc2c0edf077a188f62d05271d5824ca3
magallegos1996 <bwcekos@gmail.com>
2020-04-28 15:47:21 -0500

    Instalacion de Vue-Cli y creacion de proyecto

---
+++ vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <NavBar/>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar";
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col">
+                <h1>Columna 1</h1>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
