27bb63761426b33b84f7073837c749a88d3848a2
magallegos1996 <bwcekos@gmail.com>
2020-04-27 20:21:00 -0500

    Configuracion de servidor y creacion de rutas

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require ('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/sb_database', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+}).then(() => console.log('BASE DE DATOS CONECTADA: ')).catch(e => console.log(e));
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/sb', require(__dirname + '/routes/routes'));
+
+//Static Files
+app.use(express.static(__dirname + '/public'));
+
+//Server is listening
+app.listen(app.get('port'), ()=>{
+   console.log('Server on port ' +  app.get('port'));
+});
