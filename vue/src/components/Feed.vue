b30928fc2bcada4e2dbe2b3a5863d8a31eea57a0
magallegos1996 <bwcekos@gmail.com>
2020-05-02 23:26:18 -0500

    Se crea el component Publicaciones y se integra a la aplicacion

--- vue/src/components/Feed.vue
+++ vue/src/components/Feed.vue
@@ -2,22 +2,22 @@
     <div v-if="publicaciones.length === 0">
         <EmptyFeed/>
     </div>
-    <div class="container mb-4 mt-4" v-else>
+    <div class="container" v-else>
         <v-btn fab dark fixed top right v-bind:color="'#F50057'" @click="irASubirImagen"><font-awesome-icon icon="plus"/></v-btn>
         <div class="row pt-4">
             <div class="col-lg-4 pt-3 pb-3" v-for="(publicacion,index) in publicaciones" v-bind:key="index">
-                <div class="card" style="width: 15rem;">
-                    <img class="card-img-top img-thumbnail mx-auto" :src="`${publicPath}img/uploads/${publicacion.nombre}`" alt="Imagen">
-                    <div class="card-body">
-                        <p class="card-title"><b>{{publicacion.titulo}}</b></p>
-                        <p class="card-text"><small class="text-muted">{{publicacion.fechaSubida}} {{publicacion.horaSubida}}</small></p>
-                        <p class="card-text"><small class="text-muted"><b>Subido por:</b> {{publicacion.subidoPor}}</small></p>
-                    </div>
-                </div>
+                <Publicacion
+                        :titulo="publicacion.titulo"
+                        :descripcion="publicacion.descripcion"
+                        :fechaSubida="publicacion.fechaSubida"
+                        :horaSubida="publicacion.horaSubida"
+                        :subidoPor="publicacion.subidoPor"
+                        :imagen="`${publicPath}img/uploads/${publicacion.nombre}`"
+                />
             </div>
         </div>
-        <div class="col-lg-12 pt-5 text-center">
-            <v-btn v-if="hasNextPage" rounded light @click="cargarMas">Ver más</v-btn>
+        <div class="col-lg-12 pt-5 text-center" v-if="hasNextPage">
+            <v-btn  rounded light @click="cargarMas">Ver más</v-btn>
         </div>
     </div>
 </template>
@@ -25,6 +25,7 @@
 <script>
 
     import EmptyFeed from "./EmptyState";
+    import Publicacion from "./Publicacion";
     import PublicacionesService from '../service/Publicaciones.service'
 
     export default {
@@ -39,6 +40,7 @@
         },
         components: {
             EmptyFeed,
+            Publicacion
         },
         async created () {
             const respuesta = await PublicacionesService.obtenerPublicaciones();
@@ -69,37 +71,8 @@
 </script>
 
 <style scoped>
-
-    .card-img-top {
-        border-radius: 25px;
-        object-fit: cover;
-        border: none;
-    }
-    .card {
-        opacity: 0.85;
-        transition: 0.3s;
-    }
     .container img {
         width: 250px;
         height: 250px;
-    }
-    .card:hover{
-        cursor: pointer;
-        box-shadow: -1px 6px 35px 3px darkgray;
-        opacity: 1;
-    }
-    .card-title{
-        font-size: 15px;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        overflow: hidden;
-    }
-    #descripcionImagen{
-        font-size: 11px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 2; /* number of lines to show */
-        -webkit-box-orient: vertical;
     }
 </style>
--- vue/src/components/Publicacion.vue
+++ vue/src/components/Publicacion.vue
@@ -1,10 +1,50 @@
 <template>
-    <h1>PUBLICACION</h1>
+    <v-hover v-slot:default="{ hover }" close-delay="200">
+        <v-card class="mx-auto" :elevation="hover ? 16 : 2">
+            <v-img :src="imagen" height="200px"></v-img>
+            <v-card-title>{{titulo}}</v-card-title>
+            <v-card-subtitle class="pb-0"><b>Subido por:</b> {{subidoPor}}</v-card-subtitle>
+            <v-card-subtitle class="text-muted align-end">{{fechaSubida}}  {{horaSubida}}</v-card-subtitle>
+            <v-card-actions>
+                <v-btn primary text>Ver</v-btn>
+                <v-btn :color="'#00BFA6'" text>Eliminar</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn icon @click="show = !show">
+                    <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                </v-btn>
+            </v-card-actions>
+            <v-expand-transition>
+                <div v-show="show">
+                    <v-divider></v-divider>
+                    <v-card-text class="text-muted" v-if="descripcion === ''">
+                        No hay una descripción
+                    </v-card-text>
+                    <v-card-text v-else>
+                        {{descripcion}}
+                    </v-card-text>
+                </div>
+            </v-expand-transition>
+        </v-card>
+    </v-hover>
 </template>
 
 <script>
     export default {
-        name: "Publicacion"
+        name: "Publicacion",
+        data() {
+            return {
+                show: false,
+                publicPath: process.env.BASE_URL
+            }
+        },
+        props: {
+            imagen: String,
+            titulo: String,
+            descripcion: String,
+            fechaSubida: String,
+            horaSubida: String,
+            subidoPor: String
+        }
     }
 </script>
 
